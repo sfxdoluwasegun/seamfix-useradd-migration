47e9c95b44cef857fba2b4431eae58abdde31a21
Iwuh Jude <jiwuh@seamfix.com>
2017-12-13 14:02:32 +0100

    combining playbooks

---
+++ files/schedule.ps1
@@ -0,0 +1,7 @@
+﻿# for windows 8 and higher versions
+
+ipmo ScheduledTasks
+$action = New-ScheduledTaskAction -Execute 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' -Argument '-ExecutionPolicy Unrestricted -NonInteractive -File C:\Tower\request_tower_configuration.ps1'
+$trigger =  New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:00:30
+$principal = New-ScheduledTaskPrincipal -UserId SYSTEM -LogonType ServiceAccount -RunLevel Highest
+Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName "callAnsibleTower" -Description "call Ansible Tower" 
